a99806e1917921e00599f014b6b65d4c0fa68587
Collins1738 <tobechikeluba@gmail.com>
2020-11-11 15:00:00 -0500

    capitalized import

--- src/pages/single-player-page.js
+++ src/pages/single-player-page.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/button";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/grid";
-import Typography from "@material-ui/core/typography";
+
 import axios from "axios";
 
 const styles = (theme) => ({
