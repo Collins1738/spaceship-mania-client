e04152b193274ad4b6bfc4f8d7b610a4a5389514
Procrascat <khalil.luke@gmail.com>
2020-11-21 18:11:05 -0600

    Added nav bar and added background gif

--- src/App.js
+++ src/App.js
@@ -8,9 +8,51 @@ import axios from "axios";
 import ChallengesPage from "./pages/challenges-page";
 import ModeSelectionPage from "./pages/mode-selection-page";
 import SinglePlayerPage from "./pages/single-player-page";
+import {
+	createMuiTheme,
+	withStyles,
+	makeStyles,
+	ThemeProvider,
+} from "@material-ui/core/styles";
 
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 axios.defaults.baseURL =
 	"https://us-central1-space-maniaa.cloudfunctions.net/api";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: "#757ce8",
+			main: "#3f50b5",
+			dark: "#002884",
+			contrastText: "#fff",
+		},
+		secondary: {
+			light: "#ff7961",
+			main: "#f44336",
+			dark: "#ba000d",
+			contrastText: "#000",
+		},
+	},
+});
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
 
 class App extends Component {
 	constructor() {
@@ -23,20 +65,59 @@ class App extends Component {
 
 	render() {
 		const { username } = this.state;
+		const classes = this.props.classes;
+		const handleClick = this.props.handleClick;
+		const anchorEl = this.props.anchorEl;
+		const handleClose = this.props.handleClose;
 		return (
 			<div className="App">
 				<div className="page">
 					<Router>
 						<div className="navbar">
+							<div className={classes.root}>
+								<AppBar position="static" style={{ backgroundColor: "black" }}>
+									<Toolbar>
+										<div>
+											<Button
+												edge="start"
+												className={classes.menuButton}
+												color="inherit"
+												aria-label="menu"
+												aria-controls="simple-menu"
+												aria-haspopup="true"
+												onClick={handleClick}
+											>
+												<MenuIcon />
+											</Button>
+
+											<Menu
+												id="simple-menu"
+												anchorEl={anchorEl}
+												keepMounted
+												open={Boolean(anchorEl)}
+												onClose={handleClose}
+											>
+												<MenuItem onClick={handleClose}>Profile</MenuItem>
+												<MenuItem onClick={handleClose}>My account</MenuItem>
+												<MenuItem onClick={handleClose}>Logout</MenuItem>
+											</Menu>
+										</div>
+										<Typography variant="h6" className={classes.title}>
+											<h2>Space Mania</h2>
+										</Typography>
+										<Link to="/login">
+											<Button color="inherit">Login</Button>
+										</Link>
+									</Toolbar>
+								</AppBar>
+							</div>
 							<Link to="/login">
 								<button>Sign In</button>
 							</Link>
 							<Link to="/mode-selection">
 								<button>Mode Selection</button>
 							</Link>
-							<button onClick={this.handleSignOut}>
-								Sign Out
-							</button>
+							<button onClick={this.handleSignOut}>Sign Out</button>
 						</div>
 						<h2>Space Mania</h2>
 						<h1>Hey {username || "Anonymous"}</h1>
@@ -48,21 +129,9 @@ class App extends Component {
 								exact
 								path="/mode-selection"
 							/>
-							<Route
-								component={ChallengesPage}
-								exact
-								path="/challenges"
-							/>
-							<Route
-								component={SinglePlayerPage}
-								exact
-								path="/single-player"
-							/>
-							<Route
-								component={ModeSelectionPage}
-								exact
-								path="/"
-							/>
+							<Route component={ChallengesPage} exact path="/challenges" />
+							<Route component={SinglePlayerPage} exact path="/single-player" />
+							<Route component={ModeSelectionPage} exact path="/" />
 						</Switch>
 					</Router>
 				</div>
@@ -83,4 +152,15 @@ class App extends Component {
 		window.location.href = "/mode-selection";
 	}
 }
-export default App;
+export default () => {
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return <App classes={classes} />;
+};
